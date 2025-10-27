a8fb2bfe0cd78b15dda4db528e4137ffe1091dbc
Aron William Bechen <williambechen@gmail.com>
2025-10-27 20:51:33 +0100

    Initial commit

---
+++ ReadMe.txt
@@ -0,0 +1,18 @@
+1. This application is a password manager. That also encrypts when a note is saved.
+
+2. On first time use the user will be asked to set a 
+  4-digit PIN code. This is used to log in to the application.
+
+3. After a PIN is set 
+
+3. When a user select the choice to make a new note
+  they get a form that will store what they write.
+
+The form contains a field for
+-Title:
+-Note:
+-Email:
+-Password:
+-NB:
+
+When a user saves the note it will be encrypted..
---
+++ main.js
@@ -0,0 +1,22 @@
+const {app, BrowserWindow} = require('electron'); 
+const url = require('url');
+const path = require('path');
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: 'Electron',
+        width: 1000,
+        height: 600,
+    });
+
+    const startUrl = url.format({
+        pathname: path.join(__dirname, 'pin-setup.html'),
+        protocol: 'file',
+    });
+
+    mainWindow.loadURL(startUrl);
+}
+
+
+
+app.whenReady().then(createMainWindow);
